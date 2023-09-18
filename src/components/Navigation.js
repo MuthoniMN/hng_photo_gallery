d434d6f37dcb19e277a15b073a60e4be122d39b0
MuthoniMN <105012834+MuthoniMN@users.noreply.github.com>
2023-09-19 01:09:20 +0300

    adding form styling plugin

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -5,7 +5,7 @@ import Avatar from "./Avatar";
 export default function Navigation() {
     return (
         <nav className="flex justify-between py-16 px-32">
-            <img src="./logo.png" />
+            <img src="./logo.png" alt="Michelle's Gallery" className="w-16 h-16"/>
             <Searchbar />
             <Avatar />
         </nav>
--- src/components/Searchbar.js
+++ src/components/Searchbar.js
@@ -2,6 +2,6 @@ import React from "react";
 
 export default function Searchbar() {
     return (
-        <input placeholder="Search for an image" class="px-8 py-4 w-2/3"/>
+        <input placeholder="Search for an image" class="w-2/3 rounded-sm border-2 border-black"/>
     )
 }
