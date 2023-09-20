9582a464a53c271ddcd5109f6aee8189ec48a5bf
MuthoniMN <105012834+MuthoniMN@users.noreply.github.com>
2023-09-20 21:20:15 +0300

    search bar

--- src/components/Searchbar.js
+++ src/components/Searchbar.js
@@ -2,6 +2,6 @@ import React from "react";
 
 export default function Searchbar() {
     return (
-        <input placeholder="Search for an image" class="w-2/3 rounded-sm border-2 border-black"/>
+        <input placeholder="Search for an image" class=""/>
     )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,10 @@ module.exports = {
     extend: {},
   },
   plugins: [
-    require('@tailwindcss/forms'),
+    require('@tailwindcss/forms')({
+      strategy: 'base', // only generate global styles
+      strategy: 'class', // only generate classes
+    }),
   ],
 }
 
