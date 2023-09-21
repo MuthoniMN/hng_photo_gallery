96b660e0b69fa066abf85e6711a45eb859290b31
Michelle Ndiangui <105012834+MuthoniMN@users.noreply.github.com>
2023-09-22 01:33:48 +0300

    fixing a bug

--- src/components/Picture.js
+++ src/components/Picture.js
@@ -5,8 +5,8 @@ import 'react-loading-skeleton/dist/skeleton.css'
 const Picture = ( {imgSource, altDesc, imgTag} ) => {
     return (
         <div className="max-w-sm overflow-hidden shadow-lg h-fit-content  flex flex-col place-items-center">
-            <img src={imgSource} alt={altDesc} className="w-full mb-3" /> || <Skeleton count={5} />
-            <span className="bg-teal-400 text-white rounded-full px-2 py-1 text-sm font-semibold w-fit-content m-4">{imgTag}</span> || <Skeleton />
+            {<img src={imgSource} alt={altDesc} className="w-full mb-3" /> || <Skeleton count={5} />}
+            {<span className="bg-teal-400 text-white rounded-full px-2 py-1 text-sm font-semibold w-fit-content m-4">{imgTag}</span> || <Skeleton />}
         </div>
     )
 }
