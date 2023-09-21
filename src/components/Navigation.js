a231a4692088c7b6659effdd7501b05eb76fc7ee
MuthoniMN <105012834+MuthoniMN@users.noreply.github.com>
2023-09-21 18:11:55 +0300

    styling changes

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -2,14 +2,12 @@ import React from "react";
 import Searchbar from "./Searchbar";
 import Login from "./Login";
 
-export default function Navigation( {loggedIn} ) {
+export default function Navigation() {
     return (
         <nav className="flex justify-between flex-wrap py-8 px-12 w-full">
-            <img src="./logo.png" alt="Michelle's Gallery" className="w-16 h-16 mx-auto md:mx-0"/>
+            <img src="./logo.png" alt="Michelle's Gallery" className="w-16 h-16"/>
             <Searchbar />
-            {{loggedIn} ? 
-                    null:  
-                    <Login />}
+            <Login />
         </nav>
     )
 }
--- src/index.js
+++ src/index.js
@@ -3,10 +3,12 @@ import ReactDOM from 'react-dom/client';
 import { Auth0Provider } from '@auth0/auth0-react';
 import './index.css';
 import App from './App';
+import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <BrowserRouter>
     <Auth0Provider
       domain={process.env.REACT_APP_AUTH_DOMAIN}
       clientId={process.env.REACT_APP_AUTH_CLIENT_ID}
@@ -16,5 +18,6 @@ root.render(
     >
       <App />
     </Auth0Provider>
+    </BrowserRouter>
   </React.StrictMode>
 );
