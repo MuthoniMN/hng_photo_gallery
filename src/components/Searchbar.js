553c4f12ac2c196be00110370e3ca7badb4e3744
MuthoniMN <105012834+MuthoniMN@users.noreply.github.com>
2023-09-22 00:04:37 +0300

    fixing grid responsiveness

--- src/components/Searchbar.js
+++ src/components/Searchbar.js
@@ -10,7 +10,7 @@ const Searchbar = ({ searchText }) => {
   }
 
   return (
-    <div className='max-w-sm rounded overflow-hidden basis-3/4 order-last md:order-none md:basis-full self-center'>
+    <div className='max-w-sm rounded overflow-hidden basis-3/4 order-last sm:order-none md:basis-full self-center'>
       <form onSubmit={onSubmit} className="w-full max-w-sm">
         <div className="flex items-center border-b border-b-2 border-teal-500 py-2">
         <input onChange={e => setText(e.target.value)} className="appearance-none bg-transparent border-none w-full text-gray-700 mr-3 py-1 px-2 leading-tight focus:outline-none" type="text" placeholder="Search Image Tag..." />
