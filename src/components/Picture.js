85c93e4a56194efc98fe2fdca172a42d5a40e9d1
MuthoniMN <105012834+MuthoniMN@users.noreply.github.com>
2023-09-21 22:08:14 +0300

    adding loading skeleton

--- src/components/Picture.js
+++ src/components/Picture.js
@@ -1,10 +1,12 @@
 import React from "react";
+import Skeleton from "react-loading-skeleton";
+import 'react-loading-skeleton/dist/skeleton.css'
 
 const Picture = ( {imgSource, altDesc, imgTag} ) => {
     return (
         <div className="max-w-sm overflow-hidden shadow-lg h-fit-content  flex flex-col place-items-center">
-            <img src={imgSource} alt={altDesc} className="w-full mb-3" />
-            <span className="bg-teal-400 text-white rounded-full px-2 py-1 text-sm font-semibold w-fit-content m-4">{imgTag}</span>
+            <img src={imgSource} alt={altDesc} className="w-full mb-3" /> || <Skeleton count={5} />
+            <span className="bg-teal-400 text-white rounded-full px-2 py-1 text-sm font-semibold w-fit-content m-4">{imgTag}</span> || <Skeleton />
         </div>
     )
 }
