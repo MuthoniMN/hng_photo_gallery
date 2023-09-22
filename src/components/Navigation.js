f862ad1b35e1a2ea29fa82e0907a9b98e005e260
Michelle Ndiangui <105012834+MuthoniMN@users.noreply.github.com>
2023-09-22 22:37:15 +0300

    navigation logo

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -3,11 +3,14 @@ import Searchbar from "./Searchbar";
 import Login from "./Login";
 import Search from "../pages/Search";
 import LogoutButton from "./Logout";
+import { useNavigate } from 'react-router-dom';
 
 export default function Navigation() {
+    const navigate = useNavigate()
+
     return (
         <nav className="flex justify-between flex-wrap py-8 px-12 w-full mb-4">
-            <img src="./logo.png" alt="Michelle's Gallery" className="w-16 h-16"/>
+            <img src="./logo.png" alt="Michelle's Gallery" className="w-16 h-16" onClick={() => navigate(`/`)} />
             <Searchbar searchText={(x) => <Search tag={x}/>} />
             <Login />
             <LogoutButton />
