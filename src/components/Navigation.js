3d7d2d856f1340332056b79356f22ab4a0f40b9c
MuthoniMN <105012834+MuthoniMN@users.noreply.github.com>
2023-09-22 13:45:34 +0300

    adding logout

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -2,6 +2,7 @@ import React from "react";
 import Searchbar from "./Searchbar";
 import Login from "./Login";
 import Search from "../pages/Search";
+import LogoutButton from "./Logout";
 
 export default function Navigation() {
     return (
@@ -9,6 +10,7 @@ export default function Navigation() {
             <img src="./logo.png" alt="Michelle's Gallery" className="w-16 h-16"/>
             <Searchbar searchText={(x) => <Search tag={x}/>} />
             <Login />
+            <LogoutButton />
         </nav>
     )
 }
