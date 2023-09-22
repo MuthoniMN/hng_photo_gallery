f2f9f9f9d6f97de6790cd03ea4a34c3b6edb3287
Michelle Ndiangui <105012834+MuthoniMN@users.noreply.github.com>
2023-09-22 21:57:46 +0300

    fixing search

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -4,7 +4,7 @@ import Picture from "./Picture";
 const Gallery = ({ images }) => {
     return (
         <div className="grid md:grid-cols-3 gap-4 sm:grid-cols-2 justify-center">
-            {images.map((image, index) => <div >{<Picture image={image} key={index}/>||  <p>No images found....</p>}</div>)}
+            {images.map((image, index) => <div >{<Picture image={image} key={index}/>}</div>) ||  <p>No images found....</p>}
         </div>
     )
 }
